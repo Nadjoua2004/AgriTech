06c254ad5d4218a37e459340399c4c2c7ef5be3f
Nesrine-TAMOURTBIR <nesrinetamourtbir500@gmail.com>
2026-05-03 00:05:19 +0100

    Fix: Force total database schema rebuild

--- services/terre_service/build.sh
+++ services/terre_service/build.sh
@@ -9,6 +9,7 @@ find . -name "*.pyc" -delete
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+rm -f db.sqlite3
 python manage.py makemigrations lands
 python manage.py migrate
 python seed_lands.py
